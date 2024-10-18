d36b683f2f49883d5f76a0f779dac62ad51a2358
adisidev <64905594+adisidev@users.noreply.github.com>
2024-10-18 17:25:55 +0800

    Add pull-executable.sh script

---
+++ tools/pull-executable.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# Directory to copy the executable
+TAG="8095809"
+
+# Function to pull a specific release
+pull_release() {
+    echo "Pulling release tag: $1"
+    wget https://github.com/mgastner/cartogram-cpp/releases/download/$1/cartogram -P ./internal/executable
+
+    # Overwrite version.txt with the release tag
+    echo $1 > ./internal/executable/release-tag.txt
+}
+
+# Function to pull the latest release
+pull_latest() {
+    echo "Pulling latest release"
+    LATEST_TAG=$(curl -s https://api.github.com/repos/mgastner/cartogram-cpp/releases/latest | grep 'tag_name' | cut -d\" -f4)
+    pull_release $LATEST_TAG
+}
+
+# Check for --latest option
+if [ "$1" == "--latest" ]; then
+    pull_latest
+else
+    pull_release $TAG
+fi
