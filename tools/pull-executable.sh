f13ec1dd302790ee803350125084ada5504aa5cd
adisidev <64905594+adisidev@users.noreply.github.com>
2024-10-28 11:15:02 +0800

    Overwrite cartogram binary, color tag red

--- tools/pull-executable.sh
+++ tools/pull-executable.sh
@@ -3,10 +3,14 @@
 # Directory to copy the executable
 TAG=$(<./internal/executable/release-tag.txt)
 
+# Define the ANSI color code for red and escape code to reset color
+RED='\033[31m'
+RESET='\033[0m'
+
 # Function to pull a specific release
 pull_release() {
-    echo "Pulling release tag: $1"
-    wget https://github.com/mgastner/cartogram-cpp/releases/download/$1/cartogram -P ./internal/executable
+    printf "\nPulling release tag: ${RED}$1${RESET}\n\n"
+    wget https://github.com/mgastner/cartogram-cpp/releases/download/$1/cartogram -O ./internal/executable/cartogram
 
     # Overwrite version.txt with the release tag
     echo $1 > ./internal/executable/release-tag.txt
@@ -14,7 +18,7 @@ pull_release() {
 
 # Function to pull the latest release
 pull_latest() {
-    echo "Pulling latest release"
+    printf "\nPulling LATEST release"
     LATEST_TAG=$(curl -s https://api.github.com/repos/mgastner/cartogram-cpp/releases/latest | grep 'tag_name' | cut -d\" -f4)
     pull_release $LATEST_TAG
 }
